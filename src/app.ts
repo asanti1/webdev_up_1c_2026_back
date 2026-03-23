fc355015a5cfdb97290778bfadfc3b49558937bc
Agustin S. <asantinaque@protonmail.com>
2026-03-23 18:58:50 -0300

    basic server structure up

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/health", (req, res) => {
+    res.send("Healthy")
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
+
+export default app;
